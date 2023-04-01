37ddfc2a50cfec8652304668547470f5edfbe8d8
Moad <itstd.4117@uob.edu.ly>
2023-04-01 21:02:53 +0200

    new start

---
+++ android/app/src/main/kotlin/com/example/lyland/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lyland
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+//مشروع التخرج برعاية رمضان
+ int start 
