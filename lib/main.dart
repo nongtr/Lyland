5abbbb1138890753516f3dad7c3953603b02b614
farajeltaira <46869339+farajeltaira@users.noreply.github.com>
2023-04-01 22:06:35 +0200

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp());
+
 //مشروع التخرج برعاية رمضان
- int start
+ //int start;
     //aa1ds
