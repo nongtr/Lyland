230ea228151e2f20b9a42e9b14c338982f100359
Moad <itstd.4117@uob.edu.ly>
2023-04-02 18:35:52 +0200

    3rd edit include adding Customer_Screen.dart

--- lib/PO_Screen.dart
+++ lib/PO_Screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-class home_page extends StatelessWidget {
-
+class ProOwnSc extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -9,8 +8,10 @@ class home_page extends StatelessWidget {
       children: [
         Container(
           color: Colors.white38,
-          child: Text(' Here Will be the home page',
-            style:TextStyle(fontSize: 50.0) ,),
+          child: Text(
+            ' Here Will be the home page',
+            style: TextStyle(fontSize: 50.0),
+          ),
         )
       ],
     );
